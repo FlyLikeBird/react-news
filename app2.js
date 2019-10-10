78aff87dae490e7f99da17ee611bcced687acc7e
FlyLikeBird <renshanhang@163.com>
2019-10-10 21:34:24 +0800

    20191010test-007

--- app2.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-console.log('hello world222');
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('hello world');
-}).listen(8081);
